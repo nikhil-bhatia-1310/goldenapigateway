7dd87afdf657d07f7c4d1de62cd0a8327cf83e8c
Nikhil Bhatia <nik.bhatia.13@gmail.com>
2023-04-02 12:43:00 +0530

    first commit

---
+++ src/test/java/com/nik/golden/apigateway/GoldenapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nik.golden.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenapigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
