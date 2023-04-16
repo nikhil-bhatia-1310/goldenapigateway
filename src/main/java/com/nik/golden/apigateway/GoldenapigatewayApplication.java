f839cb46776cdf4cc07b56dd656ac16d3e060abc
Nikhil Bhatia <nik.bhatia.13@gmail.com>
2023-04-16 13:28:43 +0530

    added cross origin

--- src/main/java/com/nik/golden/apigateway/GoldenapigatewayApplication.java
+++ src/main/java/com/nik/golden/apigateway/GoldenapigatewayApplication.java
@@ -3,9 +3,11 @@ package com.nik.golden.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@CrossOrigin("http://localhost:3000/")
 public class GoldenapigatewayApplication {
 
 	public static void main(String[] args) {
